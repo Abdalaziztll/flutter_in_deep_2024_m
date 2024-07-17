8eeda6c817d5380535db9ae5557adaad4c3ad53a
AbdTll <abdalaziztll992010@gmail.com>
2024-07-17 07:59:33 -0700

    fix issue

---
+++ lib/uplode/file_uplode_example.dart
@@ -0,0 +1,83 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File? image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            image == null ? FlutterLogo() : Image.file(image!),
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+
+            // Text(
+            //   '$_counter',
+            //   style: Theme.of(context).textTheme.headlineMedium,
+            // ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+          if (result != null) {
+            late File file;
+            setState(() {
+              file = File(result.files.single.path!);
+              image = file;
+            });
+
+            Map<String, dynamic> temp = {
+              'image': [
+                await MultipartFile.fromFile(file.path, filename: 'name.png')
+              ],
+            };
+
+            // UserModel user = UserModel(email: 'email', phone: '03213213');
+
+            // temp.addAll(user.toMap());
+
+            var data = FormData.fromMap(temp);
+
+            await Dio().post('path', data: data);
+          } else {
+            // User canceled the picker
+          }
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
